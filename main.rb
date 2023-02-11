9f6feca2127528b2483da476ebef15f302331e32
kihno <stistyle13@gmail.com>
2023-02-11 15:18:38 -0800

    Complete game loop

--- main.rb
+++ main.rb
@@ -4,34 +4,31 @@ end
 
 class CodeMaker
   include AllColors
-  attr_reader :generate_code, :code
-  attr_accessor :check_code
+  attr_reader :code, :correct_color, :correct_location
 
   def generate_code
     @code = [COLORS.sample, COLORS.sample, COLORS.sample, COLORS.sample]
   end
 
   def check_code(guess)
-    p @code
-    p guess
-    intersection = @code - guess
-    p intersection
-    color = 4 - intersection.length
-    location = 0
-    p color
-    p location
+    @correct_color = 0
+    @correct_location = 0
+    code_match = @code.map(&:clone)
 
-    @code.each_with_index do |code_color, index|
-      if code_color == guess[index]
-        location += 1
-        color -= 1
+    guess.each do |guess_color|
+      if code_match.include?(guess_color)
+        index = code_match.index(guess_color)
+        code_match.delete_at(index)
+        @correct_color += 1
       end
     end
 
-    p "Secret Code: #{@code}"
-    p "Your Guess: #{guess}"
-    p "Correct Color: #{color}"
-    p "Correct Location: #{location}"
+    @code.each_with_index do |code_color, index|
+      if code_color == guess[index]
+        @correct_location += 1
+        @correct_color -= 1
+      end
+    end
   end
 end
 
@@ -64,42 +61,64 @@ class CodeBreaker
   end
 
   def guess
-    puts "Guess first color."
+    puts "Guess first color:"
     guess_one = gets.chomp.downcase
-    validate_guess(guess_one) 
-    puts "Guess second color."
+    validate_guess(guess_one)
+    puts "Guess second color:"
     guess_two = gets.chomp.downcase
-    validate_guess(guess_two) 
-    puts "Guess third color."
+    validate_guess(guess_two)
+    puts "Guess third color:"
     guess_three = gets.chomp.downcase
-    validate_guess(guess_three) 
-    puts "Guess fourth color."
+    validate_guess(guess_three)
+    puts "Guess fourth color:"
     guess_four = gets.chomp.downcase
-    validate_guess(guess_four) 
+    validate_guess(guess_four)
   end
 end
 
 class Game
   def game_start
     @hal = CodeMaker.new
-    @dave = CodeBreaker.new 
-    @turns = 12
+    @dave = CodeBreaker.new
     @gameover = false
     @hal.generate_code
     game_loop
   end
 
-  def game_loop
-    while @dave.turns > 0
-      @dave.guess
-      @hal.check_code(@dave.code_guess)
-      @dave.clear_guess
+  def welcome_message
+    puts "Welcome to Mastermind. You have 12 guesses to break the super secret code. The secret code is 4 colors from the following options: red, blue, yellow, green, white, or black. Colors may repeat. Enter your guess."
+  end
+
+  def response
+    if @hal.correct_location == 4
+      @gameover = true
+      puts "Your guess: #{@dave.code_guess}"
+      puts "Secret Code: #{@hal.code}"
+      puts "You broke the code!"
+    elsif @dave.turns == 1
+      @gameover = true
+      puts "Your guess: #{@dave.code_guess}"
+      puts "Secret Code: #{@hal.code}"
+      puts "Mission failed. You couldn't hack it."
+    else
+      puts "Your guess: #{@dave.code_guess}"
+      puts "Correct Color: #{@hal.correct_color}"
+      puts "Correct Location: #{@hal.correct_location}"
       @dave.turns -= 1
       puts "You have #{@dave.turns} guesses left."
+    end
+  end
+
+  def game_loop
+    welcome_message
+    until @gameover
+      @dave.guess
+      @hal.check_code(@dave.code_guess)
+      response
+      @dave.clear_guess
     end
   end
 end
 
 new_game = Game.new
 new_game.game_start
-
